ad1ed51b2bc828fc22101d9204712fa427e9f76e
zukonake <zukonake@protonmail.com>
2017-07-28 04:31:54 +0200

    More improvements

--- euler-003.hs
+++ euler-003.hs
@@ -1,8 +1,8 @@
 import Data.List
 import Data.Maybe
 
-isFactor :: Int -> Int -> Bool
-isFactor x y = (y `mod` x) == 0
+isFactorOf :: Int -> Int -> Bool
+isFactorOf x y = (y `mod` x) == 0
 
 factors :: Int -> [Int]
 factors n = filter (\x -> x `isFactor` n) (takeWhile (<= n ) [1..])
@@ -11,7 +11,7 @@ isPrime :: Int -> Bool
 isPrime n = (length (factors n)) == 2
 
 smallestPrimeFactor :: Int -> Int
-smallestPrimeFactor n = fromJust $ find (\x -> (x `isFactor` n) && (isPrime x)) [2..n]
+smallestPrimeFactor n = fromJust $ find (\x -> (x `isFactorOf` n) && (isPrime x)) [2..n]
 
 primeFactors :: Int -> [Int]
 primeFactors 1 = []
