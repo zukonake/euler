3371152fac09deb150b5591c5f523a085ae0f832
zukonake <zukonake@protonmail.com>
2017-07-28 16:24:56 +0200

    005 improvements

--- euler-005.hs
+++ euler-005.hs
@@ -1,13 +1,14 @@
 import Data.List
+import Data.Maybe
 
 isFactorOf :: Int -> Int -> Bool
 isFactorOf x y = (y `mod` x) == 0
 
 areFactorsOf :: [Int] -> Int -> Bool
 areFactorsOf [] _ = True
-areFactorsOf list x = ((head list) `isFactorOf` x) && (tail list) `areFactorsOf` x
+areFactorsOf (x:xs) n = (x `isFactorOf` n) && xs `areFactorsOf` n
 
-smallestMultiple :: [Int] -> Maybe Int
-smallestMultiple n = find (\x -> n `areFactorsOf` x) [1..]
+smallestMultiple :: [Int] -> Int
+smallestMultiple n = fromJust (find (\x -> n `areFactorsOf` x) [1..])
 
-main = print $ smallestMultiple [1..20]
+main = print $ smallestMultiple [1..10]
